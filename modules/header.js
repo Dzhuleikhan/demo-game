df15510d12527e64ab2249a718be40e7dde3d0c3
dzhuleikhan <dzhuleikhan@gmail.com>
2024-07-22 16:06:18 +0300

    Menu mobile done

--- modules/header.js
+++ modules/header.js
@@ -2,10 +2,15 @@ const headerLangBtn = document.querySelector(".header-lang-btn");
 const headerLangList = document.querySelector(".header-lang-list");
 const burgerBtn = document.querySelector(".burger");
 const sideMenu = document.querySelector(".sidemenu");
+const menuOverlay = document.querySelector(".menu-overlay");
 
 if (sideMenu) {
   if (window.innerWidth >= 576) {
     sideMenu.classList.add("is-open");
+  }
+
+  if (sideMenu.classList.contains("is-open")) {
+    menuOverlay.classList.add("is-visible");
   }
 }
 
@@ -25,5 +30,10 @@ if (burgerBtn) {
   burgerBtn.addEventListener("click", () => {
     sideMenu.classList.toggle("is-open");
     document.body.classList.toggle("scroll-lock");
+    if (sideMenu.classList.contains("is-open")) {
+      menuOverlay.classList.add("is-visible");
+    } else {
+      menuOverlay.classList.remove("is-visible");
+    }
   });
 }
