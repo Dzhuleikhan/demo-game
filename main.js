4a36e708a2bfd0ffdada11a6c9dd2b08513c557a
dzhuleikhan <dzhuleikhan@gmail.com>
2024-07-25 17:11:41 +0300

    Phone tab form done

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import "./style.css";
+import "intl-tel-input/build/css/intlTelInput.css";
 
 import "./modules/header";
 import "./modules/game";
 import "./modules/sidebar";
 import "./modules/language";
 import "./modules/mainModal";
+import "./modules/telInput";
