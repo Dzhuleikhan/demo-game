72077ce68a63cec42ca24427e7999a7100e20400
dzhuleikhan <dzhuleikhan@gmail.com>
2025-01-16 19:45:55 +0300

    Tir 1-2 Update

--- modules/formAuth.js
+++ modules/formAuth.js
@@ -3,6 +3,7 @@ import { authIti } from "./itiTelInput";
 import { hiddenSelect } from "./hiddenSelect";
 import { getUrlParameter, updateUrl } from "./params";
 import { newDomain } from "./fetchingDomain";
+import { checkTir1CurrencyMatch } from "./modalCurrency";
 
 // | AUTH FORM VALIDATION AND SUBMITTING
 
@@ -516,6 +517,8 @@ function submitForm(form, newDomain) {
       formbtn.disabled = true;
       formbtn.classList.add("loading");
     }
+
+    formData.bonus = checkTir1CurrencyMatch(formData.currency, formData.bonus);
 
     let lang = localStorage.getItem("preferredLanguage");
     let cid = getUrlParameter("cid");
