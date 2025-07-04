9cecd1abbf1fef7febfaf9e359d36a2f6865c2f9
dzhuleikhan <dzhuleikhan@gmail.com>
2025-07-04 18:14:53 +0400

    Excluded "RU", "MX", "CL", "CO", "TH", "ID" and changed lang SE to SV

--- modules/geoLocation.js
+++ modules/geoLocation.js
@@ -5,12 +5,9 @@ export async function getLocation() {
     "https://apiip.net/api/check?accessKey=0439ba6e-6092-46c2-9aeb-8662065bc43c";
   let response = await fetch(url);
   let data = await response.json();
-  localStorage.setItem("preferredLanguage", data.countryCode.toLowerCase());
 
   return data;
 }
-
-export const geoData = await getLocation();
 
 export const getSupportedLanguage = (countryCode) => {
   if (countryCode in countryLanguagesMap) {
@@ -23,3 +20,9 @@ export const getSupportedLanguage = (countryCode) => {
   }
   return "en";
 };
+
+export const geoData = await getLocation();
+localStorage.setItem(
+  "preferredLanguage",
+  getSupportedLanguage(geoData.countryCode),
+);
