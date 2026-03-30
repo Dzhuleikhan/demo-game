f3df85f3675273293dfa0aa7a84c3f13aa6d3522
dzhuleikhan <dzhuleikhan@gmail.com>
2026-03-30 15:54:50 +0400

    removed cdn

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://landing-res.b-cdn.net/goldbet/ndb2/",
+  base: "",
   esbuild: {
     supported: {
       "top-level-await": true,
