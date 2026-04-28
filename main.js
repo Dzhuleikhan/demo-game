88e619cad8f4d45c854cd4985493182d6b4479e9
dzhuleikhan <dzhuleikhan@gmail.com>
2026-04-28 20:17:23 +0400

    Disposable emails validation

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "intl-tel-input/build/css/intlTelInput.css";
 
 import "./modules/geoLocation";
 import "./modules/itiTelInput";
+import "./modules/disposableEmail";
 import "./modules/marquee";
 // import "./modules/hiddenSelect";
 import "./modules/formAuth";
--- modules/formSocials.js
+++ modules/formSocials.js
@@ -7,6 +7,7 @@ import { hiddenSelect } from "./hiddenSelect.js";
 import { newDomain } from "./fetchingDomain.js";
 import { checkTir1CurrencyMatch } from "./modalCurrency.js";
 import { getSupportedLanguage } from "./geoLocation.js";
+import { isDisposableEmail } from "./disposableEmail.js";
 
 // | SHOWING BONUS BASED ON PARAMS
 
@@ -111,7 +112,7 @@ formModals.forEach((modal) => {
             .querySelector(".not-valid-icon")
             .classList.add("hidden");
           formGroupEmail.classList.remove("not-valid");
-        } else if (emalInput.value.match(emailRegEx)) {
+        } else if (emalInput.value.match(emailRegEx) && !isDisposableEmail(emalInput.value)) {
           formStepBtnNext.disabled = false;
           formGroupEmail
             .querySelector(".not-valid-icon")
@@ -127,7 +128,7 @@ formModals.forEach((modal) => {
       });
 
       emalInput.addEventListener("input", () => {
-        if (emalInput.value.match(emailRegEx)) {
+        if (emalInput.value.match(emailRegEx) && !isDisposableEmail(emalInput.value)) {
           formStepBtnNext.disabled = false;
         }
       });
@@ -199,7 +200,7 @@ formModals.forEach((modal) => {
             if (tab === "email") {
               formGroupPhone.classList.remove("not-valid");
               phoneInput.value = "";
-              if (emalInput.value != "" && emalInput.value.match(emailRegEx)) {
+              if (emalInput.value != "" && emalInput.value.match(emailRegEx) && !isDisposableEmail(emalInput.value)) {
                 formStepBtnNext.disabled = false;
               } else {
                 formStepBtnNext.disabled = true;
