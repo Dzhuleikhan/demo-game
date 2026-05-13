905dcc022be5e952136f6a65171545b3c6ffc934
dzhuleikhan <dzhuleikhan@gmail.com>
2026-05-13 20:33:46 +0400

    cdn url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://landing-res.b-cdn.net/goldbet/ndb/",
+  base: "https://landing-res.b-cdn.net/goldbet/ndb/10667/",
   esbuild: {
     supported: {
       "top-level-await": true,
