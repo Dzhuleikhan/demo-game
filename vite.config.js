7e5d193d3eb19b22f45e0ef0eb907559527b4d61
dzhuleikhan <dzhuleikhan@gmail.com>
2026-07-10 02:47:03 +0400

    New NDB Multigeo 20 dollars

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://landing-res.b-cdn.net/goldbet/ndb/multigeo/30/",
+  base: "https://landing-res.b-cdn.net/goldbet/ndb/multigeo/20/",
   esbuild: {
     supported: {
       "top-level-await": true,
